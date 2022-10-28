825d022353d172b63b35cb3e90ee397c7b22466b
Elizabethsc15 <salazarkarla150@gmail.com>
2022-10-28 12:55:04 -0500

    practica de los divisores js

---
+++ js4.js
@@ -0,0 +1,38 @@
+function divisores(numero){
+    let total=0
+
+    for(let i=1; i<=numero;i++){
+        if(numero % i==0){
+            total ++;
+        }
+    }
+    return total;
+}
+
+console.log("Total de divisores", divisores(32));
+
+const divisoresV2 = function(numero){
+    let total=0;
+
+    for(let i=1; i<=numero;i++){
+        if(numero % i==0){
+            total++
+        }
+    }
+    return total
+};
+
+console.log("Total divisiores (v2)",divisoresV2);
+
+function calcular(){
+    let numero1 = 
+    parseInt(document.getElementById("numero").value);
+    console.log (numero1);
+
+    let resultado=
+    document.getElementById("resultado");
+
+    resultado.value=divisores(numero1);
+
+    console.log(numero1)
+};
